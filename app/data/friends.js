5d9f9c3096d5f91d66796fc80ee787238260ee45
B Kelly <berenicekelly808@gmail.com>
2019-10-26 23:30:23 -0700

    adding friends array

---
+++ app/data/friends.js
@@ -0,0 +1,29 @@
+var friendArray = [
+    {
+        name: "Sam",
+        pic: "https://media.gettyimages.com/photos/portrait-of-smiling-young-man-wearing-eyeglasses-picture-id985138634?s=612x612",
+        scores: [2, 4, 1, 5, 1, 1, 5, 5, 2, 7]
+    },
+    {
+        name: "Jack",
+        pic: "https://www.nicepng.com/png/detail/340-3400381_smiling-man-smiling-man-face-png.png",
+        scores: [3, 5, 2, 1, 4, 2, 2, 2, 5, 1]
+    },
+    {
+        name: "Ken",
+        pic: "https://thoughtcatalog.files.wordpress.com/2014/05/shutterstock_101032591.jpg?w=584&h=389",
+        scores: [1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
+    },
+    {
+        name: "Lucy",
+        pic: "https://s3.amazonaws.com/freestock-prod/450/freestock_131197091.jpg",
+        scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+    },
+    {
+        name: "Beyonce",
+        pic: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2F7e%2F2f%2F5c%2F7e2f5c1f4e312095e8c1bcbb6e187044.jpg&f=1&nofb=1",
+        scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    }
+];
+
+module.exports = friendArray;
